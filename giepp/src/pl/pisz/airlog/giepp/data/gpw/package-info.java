4a9c77ae56878cee432f32ebd5248ef46b0e6a8d
robin92pl <robin92pl@gmail.com>
2013-10-29 18:45:13 +0100

    Created package-info files for directories lacking it.

---
+++ giepp/src/pl/pisz/airlog/giepp/data/gpw/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains parser and downloader specific to <a href="http://gpw.pl">GPW</a> website.
+ *
+ * @author Rafal, Joanna
+ */
+ 
+package pl.pisz.airlog.giepp.data.gpw;
+
