30d23e6cdce460b8a3952a0950f560b149d53396
robin92pl <robin92pl@gmail.com>
2014-01-27 21:28:59 +0100

    Dodano dokumentację.

---
+++ giepp/src/pl/pisz/airlog/giepp/plot/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Zawiera klasy wspomagające rysowanie wykresów.
+ *
+ * @author Joanna
+ */
+ 
+package pl.pisz.airlog.giepp.plot;
---
+++ giepp/src/pl/pisz/airlog/giepp/xml/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Narzędzia umożliwiające uniwersalną (niezależną od platformy) serializację do XML.
+ * @author Rafal
+ */
+ 
+package pl.pisz.airlog.giepp.xml;
